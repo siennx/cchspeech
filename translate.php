498f344738b365f5e55c47e1ab4ced4afeb92609
siennx <siennx@gmail.com>
2018-06-09 17:59:49 +0800

    microsoft translator php repo

---
+++ translate.php
@@ -0,0 +1,87 @@
+<?php
+
+// NOTE: Be sure to uncomment the following line in your php.ini file.
+// ;extension=php_openssl.dll
+
+// **********************************************
+// *** Update or verify the following values. ***
+// **********************************************
+$text = "";
+
+if(is_array($_POST)&&count($_POST)>0)//先判斷是否有值   
+	{
+		$text = $_POST['txt'];
+	
+	}
+elseif(is_array($_GET)&&count($_GET)>0)
+	{
+		$text = $_GET['txt'];
+    } 
+
+if($text=="")
+	{
+		echo "There is nothing to be processed";//txt parameter 不存在  
+		exit;		
+	}
+	
+	
+
+// Replace the subscriptionKey string value with your valid subscription key.
+$key = 'bb96f19a69b14e2aadae2cebbd91e76f';
+
+$host = "https://api.cognitive.microsofttranslator.com";
+$path = "/translate?api-version=3.0";
+
+// Translate to German and Italian.
+$params = "&to=en&to=zh-Hant";
+
+//$text = $_POST['txt'];
+
+if (!function_exists('com_create_guid')) {
+  function com_create_guid() {
+    return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+        mt_rand( 0, 0xffff ),
+        mt_rand( 0, 0x0fff ) | 0x4000,
+        mt_rand( 0, 0x3fff ) | 0x8000,
+        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+    );
+  }
+}
+
+function Translate ($host, $path, $key, $params, $content) {
+
+    $headers = "Content-type: application/json\r\n" .
+        "Content-length: " . strlen($content) . "\r\n" .
+        "Ocp-Apim-Subscription-Key: $key\r\n" .
+        "X-ClientTraceId: " . com_create_guid() . "\r\n";
+
+    // NOTE: Use the key 'http' even if you are making an HTTPS request. See:
+    // http://php.net/manual/en/function.stream-context-create.php
+    $options = array (
+        'http' => array (
+            'header' => $headers,
+            'method' => 'POST',
+            'content' => $content
+        )
+    );
+    $context  = stream_context_create ($options);
+    $result = file_get_contents ($host . $path . $params, false, $context);
+    return $result;
+}
+
+$requestBody = array (
+    array (
+        'Text' => $text,
+    ),
+);
+$content = json_encode($requestBody);
+
+$result = Translate ($host, $path, $key, $params, $content);
+
+// Note: We convert result, which is JSON, to and from an object so we can pretty-print it.
+// We want to avoid escaping any Unicode characters that result contains. See:
+// http://php.net/manual/en/function.json-encode.php
+$json = json_encode(json_decode($result), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+echo $json;
+?>
